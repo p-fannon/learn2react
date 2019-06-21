1ce49d0e36687cf4007d003c2eb050b031379a52
cfspfannon <pfannon@coolfiresolutions.com>
2019-06-21 16:36:57 -0500

    Added foundational code to Main.js and index.js

---
+++ src/app/Main.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+// import About from './pages/About'
+// import Contact from './pages/Contact'
+// import Home from './pages/Home'
+// import Portfolio from './pages/Portfolio'
+
+export default class Main extends React.Component {
+  render () {
+    return (
+      <div className="wrapper">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+
+              <li>
+                <a href="/about">About</a>
+              </li>
+
+              <li>
+                <a href="/portfolio">Portfolio</a>
+              </li>
+
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        {/* <Route exact={true} path="/" component={Home}/> */}
+        {/* <Route path="/about" component={About}/> */}
+        {/* <Route path="/contact" component={Contact}/> */}
+        {/* <Route path="/portfolio" component={Portfolio}/> */}
+      </div>
+    )
+  }
+}
