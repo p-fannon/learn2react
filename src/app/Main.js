b0ddc4be33a2577b96a3049035e7692b3f9d6d70
cfspfannon <pfannon@coolfiresolutions.com>
2019-06-23 14:12:10 -0500

    Added foundational styled components to Nav.js, refactored Main.js to make use of future components

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
-// import About from './pages/About'
-// import Contact from './pages/Contact'
-// import Home from './pages/Home'
-// import Portfolio from './pages/Portfolio'
+//Import pages
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Portfolio from './pages/Portfolio'
 
 export default class Main extends React.Component {
   render () {
     return (
       <div className="wrapper">
-        <header>
+        {/* <header>
           <nav>
             <ul>
               <li>
@@ -30,12 +31,12 @@ export default class Main extends React.Component {
               </li>
             </ul>
           </nav>
-        </header>
+        </header> */}
 
-        {/* <Route exact={true} path="/" component={Home}/> */}
-        {/* <Route path="/about" component={About}/> */}
-        {/* <Route path="/contact" component={Contact}/> */}
-        {/* <Route path="/portfolio" component={Portfolio}/> */}
+        <Route exact={true} path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/contact" component={Contact}/>
+        <Route path="/portfolio" component={Portfolio}/>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
+import { injectGlobal } from 'styled-components'
+
 import Main from './App/Main'
 
+//Global style
+injectGlobal`
+html,
+body,
+#app,
+.wrapper {
+  min-height: 100vh;
+  height: 100%;
+}
+
+html {
+  box-sizing: border-box;
+  font-size: 100%;
+}
+
+* {
+  &,
+  &::after,
+  &::before {
+    box-sizing: inherit;
+  }
+}
+
+body {
+  padding: 0;
+  margin: 0;
+  font: 1rem / 1.414 arial, sans-serif;
+}
+`
+
 const container = document.querySelector('#app')
+const wrapper = document.getElementById('app')
 
 const App = () => (
   <BrowserRouter>
@@ -12,4 +45,4 @@ const App = () => (
   </BrowserRouter>
 )
 
-ReactDOM.render(<App />, container)
+ReactDOM.render(<App />, wrapper)
